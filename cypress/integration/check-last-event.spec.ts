3c551a41de48c13b4e3414b01a9d0cc8f92b6454
AndreLucyo2 <andre.lucyo2@gmail.com>
2022-04-02 14:48:58 -0300

    criado classe

--- cypress/integration/check-last-event.spec.ts
+++ cypress/integration/check-last-event.spec.ts
@@ -1,5 +1,24 @@
+//TDD
+// RED - Falha o teste 
+// GREEN - justa os erros para rodar
+// REFACTORY - refatora para passar no teste
+
+
+//----------------------------------------------------------------
+//A classe diz a intenção , o que eu quero fazer 
+// Ometo tem o objetivo 
+class CheckLastEventStatus {
+
+
+}
+
+
+//TESTE --------------------------------------------
 describe('CheckLastEventStatus', () => {
     it('Should get last evet data', () => {
+        const checkLastEventStatus = new CheckLastEventStatus()
+
+
 
     });
 
